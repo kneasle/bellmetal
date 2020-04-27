0680ce3556e764951f3ecc2f4adc3a52a471e5dd
Kneasle <kneasle@gmail.com>
2020-04-27 18:47:08 +0100

    Allow TouchIterators to be chained together

--- bellmetal/src/touch_iterator.rs
+++ bellmetal/src/touch_iterator.rs
@@ -1,6 +1,8 @@
 use crate::{ Bell, Stage, Change, Touch, Transposition, MultiplicationIterator };
 use crate::proving::fill_from_iterator;
 
+use core::iter::Chain;
+
 pub trait TouchIterator<'a> {
     type BellIter : Iterator<Item = Bell>;
     type RuleoffIter : Iterator<Item = usize>;
@@ -23,6 +25,10 @@ pub trait TouchIterator<'a> {
         TransfiguredTouchIterator::new (self, transposition)
     }
 
+    fn chain<I : TouchIterator<'a>> (&'a self, other : &'a I) -> ChainedTouchIterator<Self, I> where Self : Sized {
+        ChainedTouchIterator::new (self, other)
+    }
+
     fn collect (self) -> Touch where Self : Sized {
         Touch::from_iterator (&self)
     }
@@ -43,6 +49,139 @@ pub trait TouchIterator<'a> {
         self.music_score_without_alloc (&mut Change::rounds (self.stage ()))
     }
 }
+
+
+
+
+
+
+pub struct CallShift<I : Iterator<Item = (usize, char)>> {
+    iter : I,
+    shift : usize
+}
+
+impl<T : Iterator<Item = (usize, char)>> Iterator for CallShift<T> {
+    type Item = (usize, char);
+
+    fn next (&mut self) -> Option<(usize, char)> {
+        match self.iter.next () {
+            Some ((i, c)) => Some ((i + self.shift, c)),
+            None => None
+        }
+    }
+
+    fn size_hint (&self) -> (usize, Option<usize>) {
+        self.iter.size_hint ()
+    }
+}
+
+
+
+pub struct RuleoffShift<I : Iterator<Item = usize>> {
+    iter : I,
+    shift : usize
+}
+
+impl<T : Iterator<Item = usize>> Iterator for RuleoffShift<T> {
+    type Item = usize;
+
+    fn next (&mut self) -> Option<usize> {
+        match self.iter.next () {
+            Some (v) => Some (v + self.shift),
+            None => None
+        }
+    }
+
+    fn size_hint (&self) -> (usize, Option<usize>) {
+        self.iter.size_hint ()
+    }
+}
+
+
+
+pub struct MethodNameShift<'a, I : Iterator<Item = (usize, &'a str)>> {
+    iter : I,
+    shift : usize
+}
+
+impl<'a, T : Iterator<Item = (usize, &'a str)>> Iterator for MethodNameShift<'a, T> {
+    type Item = (usize, &'a str);
+
+    fn next (&mut self) -> Option<(usize, &'a str)> {
+        match self.iter.next () {
+            Some ((i, s)) => Some ((i + self.shift, s)),
+            None => None
+        }
+    }
+
+    fn size_hint (&self) -> (usize, Option<usize>) {
+        self.iter.size_hint ()
+    }
+}
+
+
+
+pub struct ChainedTouchIterator<'a, I : TouchIterator<'a>, J : TouchIterator<'a>> {
+    first : &'a I,
+    second : &'a J,
+    first_len : usize
+}
+
+impl<'a, I : TouchIterator<'a>, J : TouchIterator<'a>> ChainedTouchIterator<'a, I, J> {
+    pub fn new (first : &'a I, second : &'a J) -> ChainedTouchIterator<'a, I, J> {
+        ChainedTouchIterator {
+            first_len : first.length (),
+            first : first,
+            second : second
+        }
+    }
+}
+
+impl<'a, I : TouchIterator<'a>, J : TouchIterator<'a>> TouchIterator<'a> for ChainedTouchIterator<'a, I, J> {
+    type BellIter = Chain<I::BellIter, J::BellIter>;
+    type RuleoffIter = Chain<I::RuleoffIter, RuleoffShift<J::RuleoffIter>>;
+    type CallIter = Chain<I::CallIter, CallShift<J::CallIter>>;
+    type MethodNameIter = Chain<I::MethodNameIter, MethodNameShift<'a, J::MethodNameIter>>;
+    type LeftoverChangeIter = J::LeftoverChangeIter;
+
+    fn bell_iter (&self) -> Self::BellIter {
+        self.first.bell_iter ().chain (self.second.bell_iter ())
+    }
+
+    fn ruleoff_iter (&self) -> Self::RuleoffIter {
+        self.first.ruleoff_iter ().chain (RuleoffShift {
+            iter : self.second.ruleoff_iter (),
+            shift : self.first_len
+        })
+    }
+
+    fn call_iter (&self) -> Self::CallIter {
+        self.first.call_iter ().chain (CallShift {
+            iter : self.second.call_iter (),
+            shift : self.first_len
+        })
+    }
+
+    fn method_name_iter (&self) -> Self::MethodNameIter {
+        self.first.method_name_iter ().chain (MethodNameShift {
+            iter : self.second.method_name_iter (),
+            shift : self.first_len
+        })
+    }
+
+    fn stage (&self) -> Stage {
+        self.first.stage ()
+    }
+
+    fn length (&self) -> usize {
+        self.first_len + self.second.length ()
+    }
+
+    fn leftover_change_iter (&self) -> Self::LeftoverChangeIter {
+        self.second.leftover_change_iter ()
+    }
+}
+
 
 
 
